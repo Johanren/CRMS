d9d1a25d0a90709217672604a1d02100a26b1ca0
Johanren <jfrengifo04@misena.edu.co>
2025-11-27 16:36:49 -0500

    base de datos

---
+++ crm (1).sql
@@ -0,0 +1,477 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-11-2025 a las 22:04:55
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `accion`
+--
+
+CREATE TABLE `accion` (
+  `id_accion` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `barrio`
+--
+
+CREATE TABLE `barrio` (
+  `id_barrio` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `ciudad_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `campana`
+--
+
+CREATE TABLE `campana` (
+  `id_campana` int(11) NOT NULL,
+  `nombre` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrera`
+--
+
+CREATE TABLE `carrera` (
+  `id_carrera` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE `ciudad` (
+  `id_ciudad` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `identificacion` varchar(50) DEFAULT NULL,
+  `nombres` varchar(80) NOT NULL,
+  `apellidos` varchar(80) DEFAULT NULL,
+  `telefono_principal` varchar(30) DEFAULT NULL,
+  `email` varchar(120) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `barrio_id` int(11) DEFAULT NULL,
+  `ciudad_id` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_leads`
+--
+
+CREATE TABLE `estado_leads` (
+  `id_estado_leads` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fuente`
+--
+
+CREATE TABLE `fuente` (
+  `id_fuente` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horario`
+--
+
+CREATE TABLE `horario` (
+  `id_horario` int(11) NOT NULL,
+  `descripcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `interes`
+--
+
+CREATE TABLE `interes` (
+  `id_interes` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `leads`
+--
+
+CREATE TABLE `leads` (
+  `id_lead` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `info_adicional` text DEFAULT NULL,
+  `carrera_id` int(11) DEFAULT NULL,
+  `horario_id` int(11) DEFAULT NULL,
+  `interes_id` int(11) DEFAULT NULL,
+  `fuente_id` int(11) DEFAULT NULL,
+  `campana_id` int(11) DEFAULT NULL,
+  `accion_id` int(11) DEFAULT NULL,
+  `barrio_id` int(11) DEFAULT NULL,
+  `ciudad_id` int(11) DEFAULT NULL,
+  `estado_leads_id` int(11) DEFAULT NULL,
+  `observaciones` text DEFAULT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medio`
+--
+
+CREATE TABLE `medio` (
+  `id_medio` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `telefono_adicional`
+--
+
+CREATE TABLE `telefono_adicional` (
+  `id_numero_adicional` int(11) NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `telefono` varchar(30) NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `codigo` varchar(30) DEFAULT NULL,
+  `nombres` varchar(80) NOT NULL,
+  `apellidos` varchar(80) DEFAULT NULL,
+  `email` varchar(120) NOT NULL,
+  `telefono` varchar(30) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT 1,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id_rol` int(11) NOT NULL,
+  `nombre_rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `accion`
+--
+ALTER TABLE `accion`
+  ADD PRIMARY KEY (`id_accion`);
+
+--
+-- Indices de la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  ADD PRIMARY KEY (`id_barrio`),
+  ADD KEY `fk_barrio_ciudad` (`ciudad_id`);
+
+--
+-- Indices de la tabla `campana`
+--
+ALTER TABLE `campana`
+  ADD PRIMARY KEY (`id_campana`);
+
+--
+-- Indices de la tabla `carrera`
+--
+ALTER TABLE `carrera`
+  ADD PRIMARY KEY (`id_carrera`);
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`id_ciudad`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `fk_cliente_barrio` (`barrio_id`),
+  ADD KEY `fk_cliente_ciudad` (`ciudad_id`),
+  ADD KEY `idx_cliente_email` (`email`),
+  ADD KEY `idx_cliente_ident` (`identificacion`);
+
+--
+-- Indices de la tabla `estado_leads`
+--
+ALTER TABLE `estado_leads`
+  ADD PRIMARY KEY (`id_estado_leads`);
+
+--
+-- Indices de la tabla `fuente`
+--
+ALTER TABLE `fuente`
+  ADD PRIMARY KEY (`id_fuente`);
+
+--
+-- Indices de la tabla `horario`
+--
+ALTER TABLE `horario`
+  ADD PRIMARY KEY (`id_horario`);
+
+--
+-- Indices de la tabla `interes`
+--
+ALTER TABLE `interes`
+  ADD PRIMARY KEY (`id_interes`);
+
+--
+-- Indices de la tabla `leads`
+--
+ALTER TABLE `leads`
+  ADD PRIMARY KEY (`id_lead`),
+  ADD KEY `fk_leads_cliente` (`cliente_id`),
+  ADD KEY `fk_leads_carrera` (`carrera_id`),
+  ADD KEY `fk_leads_horario` (`horario_id`),
+  ADD KEY `fk_leads_interes` (`interes_id`),
+  ADD KEY `fk_leads_fuente` (`fuente_id`),
+  ADD KEY `fk_leads_campana` (`campana_id`),
+  ADD KEY `fk_leads_accion` (`accion_id`),
+  ADD KEY `fk_leads_barrio` (`barrio_id`),
+  ADD KEY `fk_leads_ciudad` (`ciudad_id`),
+  ADD KEY `idx_leads_user` (`user_id`),
+  ADD KEY `idx_leads_estado` (`estado_leads_id`);
+
+--
+-- Indices de la tabla `medio`
+--
+ALTER TABLE `medio`
+  ADD PRIMARY KEY (`id_medio`);
+
+--
+-- Indices de la tabla `telefono_adicional`
+--
+ALTER TABLE `telefono_adicional`
+  ADD PRIMARY KEY (`id_numero_adicional`),
+  ADD KEY `idx_telefono_cliente` (`cliente_id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `fk_user_rol` (`rol_id`);
+
+--
+-- Indices de la tabla `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `accion`
+--
+ALTER TABLE `accion`
+  MODIFY `id_accion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  MODIFY `id_barrio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `campana`
+--
+ALTER TABLE `campana`
+  MODIFY `id_campana` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `carrera`
+--
+ALTER TABLE `carrera`
+  MODIFY `id_carrera` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  MODIFY `id_ciudad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_leads`
+--
+ALTER TABLE `estado_leads`
+  MODIFY `id_estado_leads` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `fuente`
+--
+ALTER TABLE `fuente`
+  MODIFY `id_fuente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `horario`
+--
+ALTER TABLE `horario`
+  MODIFY `id_horario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `interes`
+--
+ALTER TABLE `interes`
+  MODIFY `id_interes` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `leads`
+--
+ALTER TABLE `leads`
+  MODIFY `id_lead` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `medio`
+--
+ALTER TABLE `medio`
+  MODIFY `id_medio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `telefono_adicional`
+--
+ALTER TABLE `telefono_adicional`
+  MODIFY `id_numero_adicional` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  ADD CONSTRAINT `fk_barrio_ciudad` FOREIGN KEY (`ciudad_id`) REFERENCES `ciudad` (`id_ciudad`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `fk_cliente_barrio` FOREIGN KEY (`barrio_id`) REFERENCES `barrio` (`id_barrio`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_cliente_ciudad` FOREIGN KEY (`ciudad_id`) REFERENCES `ciudad` (`id_ciudad`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `leads`
+--
+ALTER TABLE `leads`
+  ADD CONSTRAINT `fk_leads_accion` FOREIGN KEY (`accion_id`) REFERENCES `accion` (`id_accion`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_barrio` FOREIGN KEY (`barrio_id`) REFERENCES `barrio` (`id_barrio`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_campana` FOREIGN KEY (`campana_id`) REFERENCES `campana` (`id_campana`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_carrera` FOREIGN KEY (`carrera_id`) REFERENCES `carrera` (`id_carrera`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_ciudad` FOREIGN KEY (`ciudad_id`) REFERENCES `ciudad` (`id_ciudad`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_estado` FOREIGN KEY (`estado_leads_id`) REFERENCES `estado_leads` (`id_estado_leads`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_fuente` FOREIGN KEY (`fuente_id`) REFERENCES `fuente` (`id_fuente`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_horario` FOREIGN KEY (`horario_id`) REFERENCES `horario` (`id_horario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_interes` FOREIGN KEY (`interes_id`) REFERENCES `interes` (`id_interes`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_leads_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `telefono_adicional`
+--
+ALTER TABLE `telefono_adicional`
+  ADD CONSTRAINT `fk_tel_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_user_rol` FOREIGN KEY (`rol_id`) REFERENCES `user_role` (`id_rol`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
