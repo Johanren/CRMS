9c97d6c8a71f1680b0e2d714787c1fac12d00c40
Jose Juan <jjmoraherrera@gmail.com>
2026-01-26 23:02:23 -0500

    agregar pagina de agradedimiento
    https://multicomputo.edu.co/gracias.php

--- php/template/frm_multicomputo/index.php
+++ php/template/frm_multicomputo/index.php
@@ -268,8 +268,15 @@
                                 .then(res => res.json())
                                 .then(resp => {
                                     console.log("Correo:", resp);
+                                    // 3. Redirigir a la página de agradecimiento
+                                    window.location.href = 'https://multicomputo.edu.co/gracias.php';
+                                })
+                                .catch(err => {
+                                    console.error("Error en correo", err);
+                                    // Si el correo falla, redirigimos de todas formas
+                                    window.location.href = 'https://multicomputo.edu.co/gracias.php';
                                 });
-                            // ----------------------------------
+                                                    // ----------------------------------
 
                         } else {
                             Swal.fire("Error", data.message, "error");
